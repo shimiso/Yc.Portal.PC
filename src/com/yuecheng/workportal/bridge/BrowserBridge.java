ce742fe906f0cfbcbee797004cb5a71cc0a16cc8
shimingsong <simgsg@163.com>
2019-11-05 14:45:36 +0800

    新增版本更新

--- src/com/yuecheng/workportal/bridge/BrowserBridge.java
+++ src/com/yuecheng/workportal/bridge/BrowserBridge.java
@@ -1,5 +1,6 @@
 package com.yuecheng.workportal.bridge;
 
+import com.yuecheng.workportal.BrowserManager;
 import com.yuecheng.workportal.screen.Capturer;
 import com.yuecheng.workportal.tools.DesktopAppUtils;
 import com.yuecheng.workportal.ui.CaptureScreenUI;
@@ -12,12 +13,16 @@ import com.yuecheng.workportal.ui.Main;
  *
  */
 public class BrowserBridge {
+	public static String versionCode = "1";
+	public static String versionName = "Dev_1.0.1";
 	//程序主界面
 	Main mainFrame;
 	//截屏后的回调函数
 	public static String shotPhotoCallback = "shotPhotoCallback('%s')";
 	//打开PC端回调回调函数
 	public static String openWinAPPCallback ="openWinAPPCallback('%s')";
+	//打开PC端回调回调函数
+	public static String getVersionCallback ="getVersionCallback('%1s','%2s')";
 	
 	public BrowserBridge(Main mainFrame) {
 		this.mainFrame = mainFrame;
@@ -51,6 +56,13 @@ public class BrowserBridge {
 	public void openLeTian() {
 		DesktopAppUtils.openLeTian();
 	}
+	
+	/**
+	 * 打开Outlook邮箱
+	 */
+	public void openOutLookEmail() {
+		DesktopAppUtils.openOutLookEmail();
+	}
 	/**
 	 * 弹出右下角提示框
 	 * @param title 标题
@@ -66,7 +78,14 @@ public class BrowserBridge {
 	 */
 	public void beginCapture(boolean hideFrameBox) {
 //		CaptureScreenUI.getInstance(mainFrame).shot(hideFrameBox); 
-		
 		Capturer.getInstance(mainFrame).beginCapture(hideFrameBox);
+	}
+	
+	/**
+	 * 获取当前版本号
+	 */
+	public void getVersion() {
+		BrowserManager.getInstance().getBrowser().
+    	executeJavaScript(String.format(BrowserBridge.getVersionCallback,versionCode,versionName));
 	}
 }
