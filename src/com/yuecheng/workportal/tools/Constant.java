f82db9e71a190ffb3d4e5f8345dfce00b84d143f
shimingsong <simgsg@163.com>
2019-11-12 17:03:42 +0800

    新增中英文国际化

---
+++ src/com/yuecheng/workportal/tools/Constant.java
@@ -0,0 +1,12 @@
+package com.yuecheng.workportal.tools;
+
+public class Constant {
+	public static final String MainFrame_Title = "MainFrame_Title";
+	public static final String Open_Item = "Open_Item";
+	public static final String Debug_Item = "Debug_Item";
+	public static final String Server_Item = "Server_Item";
+	public static final String Exit_Item = "Exit_Item";
+	public static final String Save_Capture = "Save_Capture";
+	public static final String OK_Capture = "OK_Capture";
+	public static final String Exit_Capture = "Exit_Capture";
+}
